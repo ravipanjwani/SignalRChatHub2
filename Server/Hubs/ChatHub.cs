bb1f0d19e145ea43b0a3f71f88f321ecb2a0a1b5
Ravi Panjwani <ravipanjwani1991@gmail.com>
2022-04-20 00:37:48 -0400

    Done

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorWebAssemblySignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", name, message);
+        }
+        
+    }
+}
